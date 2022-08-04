3c21ea1f10496f33e5967a50bb9bf357a286db4b
AlaaAlmomani0 <alaaalmomani157@gmail.com>
2022-08-04 08:21:47 +0300

    constructor

--- app.js
+++ app.js
@@ -228,6 +228,76 @@ array.push(
 
 for (let i = 0; i < array.length; i++) {
   console.log(`Employee name: ${array[i]["fullName"]}`);
+}
+//////////////////////////////////////////////////////////
+const employees = [];
+function Employee(employeeId, fullName, department, level, imageURL) {
+  this.employeeId = 999;
+  this.fullName = fullName;
+  this.department = department;
+  this.level = level;
+  this.imageURL = imageURL;
 
-  
+  (this.uniqueId = function () {
+    this.employeeId += numberId;
+    numberId++;
+    return this.employeeId;
+  }),
+    (this.salary = function () {
+      let min = 0;
+      let max = 0;
+      if (this.level === "Senior") {
+        min = 1500;
+        max = 2000;
+        return Math.floor(Math.random() * (max - min)) + min;
+      } else if (this.level === "Mid-Senior") {
+        min = 1000;
+        max = 1500;
+        return Math.floor(Math.random() * (max - min)) + min;
+      } else if (this.level === "Junior") {
+        min = 500;
+        max = 1000;
+        return Math.floor(Math.random() * (max - min)) + min;
+      }
+    });
+  employees.push(this);
+}
+const ghaziSamer = new Employee("Ghazi Samer", "Administration", "Senior", "");
+const lanaAli = new Employee("Lana Ali", "Finance", "Senior", "");
+const tamaraAyoub = new Employee("Tamara Ayoub", "Marketing", "Senior", "");
+const safiWalid = new Employee(
+  "Safi Walid	",
+  "Administration",
+  "Mid-Senior",
+  ""
+);
+const omarZaid = new Employee("Omar Zaid", "Development", "Senior", "");
+const ranaSaleh = new Employee("Rana Saleh", "Development", "Senior", "");
+const hadiAhmad = new Employee("Hadi Ahmad", "Finance", "Mid-Senior", "");
+
+for (let i = 0; i < employees.length; i++) {
+  console.log(`Employee name: ${employees[i].fullName} `);
+  console.log(`Department: ${employees[i].department} `);
+  console.log(`Employee salary: ${employees[i].salary()} `);
+}
+
+Employee.prototype.writeToHTML = function () {
+  document.write(
+    `<div class="inner-employees-cards-section">
+        <div class="test">
+        <img src="${this.imageUrl}" alt="employee pfp" />
+        </div>
+        <p class="employee-name">${this.fullName}</p>
+      
+        <div class="to-hide">
+        <p class="to-be-hidden">Department / ${this.department}</p>
+        <p class="to-be-hidden">${this.level}</p>
+        <p class="to-be-hidden">Salary / ${this.salary()} JD</p>
+        </div>
+      </div>`
+  );
+};
+
+for (let j = 0; j < employees.length; j++) {
+  employees[j].writeToHTML();
 }
